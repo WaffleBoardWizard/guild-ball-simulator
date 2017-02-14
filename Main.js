3de239f0b09a7709457c1c0083ef426c87738f34
unknown <apheim@gmail.com>
2017-02-14 10:38:33 -0600

    Refactored Menu to not depend on certian instances

--- Main.js
+++ Main.js
@@ -37,22 +37,64 @@ var boardState = {
 
 
 var menuButtons = [{
-        Name: "Jog",
-        Icon: FontAwesomeIcons.arrowright,
-        click: function() {
-            console.log("I was clicked");
-            return true;
-        }
-    },
-    {
-        Name: "Undo",
-        Icon: FontAwesomeIcons.undo,
-        click: function() {
-            console.log("undo");
-            return true;
-        }
+    Name: "Jog",
+    Icon: FontAwesomeIcons.arrowright,
+    click: function() {
+        console.log("I was clicked");
+        return true;
     }
-];
+}, {
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+},{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function(btn) {
+        console.log("undo");
+        toggleMenu(btn, "up", menuButtons);
+        return false;
+    }
+},
+{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+},{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+},{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+},{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+},{
+    Name: "Undo",
+    Icon: FontAwesomeIcons.undo,
+    click: function() {
+        console.log("undo");
+        return true;
+    }
+}];
 
 var gamePieces = [];
 
@@ -61,7 +103,7 @@ function init() {
     stage = new createjs.Stage("demoCanvas");
     stage.enableMouseOver();
     stage.mouseMoveOutside = true;
-    var numberOfPieces = 5;
+    var numberOfPieces = 1;
 
     for (i = 0; i < numberOfPieces; i++) {
 
@@ -95,6 +137,7 @@ function init() {
         gamePieceGraphic.graphics.beginFill("red").drawCircle(0, 0, 10);
         gamePieceGraphic.x = x;
         gamePieceGraphic.y = y;
+        gamePieceGraphic.setBounds(x, y, 20, 20);
         gamePieceGraphic.hitArea = hit;
         gamePiece.canvasReference = gamePieceGraphic;
         gamePieces.push(gamePiece);
@@ -102,7 +145,9 @@ function init() {
 
         gamePieceGraphic.on("click",
             function(evt) {
-                toggleMenu(evt, "circle", menuButtons);
+                getGamePieceForEvent(evt, function(piece) {
+                    toggleMenu(piece.canvasReference, "circle", menuButtons);
+                });
             });
 
         gamePieceGraphic.on("dblclick", movementEvent);
@@ -111,7 +156,7 @@ function init() {
         stage.addChild(gamePieceGraphic);
         stage.update();
     }
-    
+
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
     stage.update();
@@ -356,113 +401,111 @@ function detectCollision(evt, gamePieces, success) {
     success(result);
 }
 
-function toggleMenu(evt, orientation, btns) {
+function toggleMenu(displayObject, orientation, btns) {
     let menuButtons = [];
 
-    getGamePieceForEvent(evt, function(piece) {
-
-        function hide(clickedIndex) {
-            menuButtons.forEach(function(btn, index) {
-                setTimeout(function() {
-                    createjs.Tween.get(btn, {
-                        loop: false
-                    }).to({
-                        x: piece.canvasReference.x,
-                        y: piece.canvasReference.y
-                    }, 1000, createjs.Ease.getPowInOut(4));
-                }, clickedIndex == index ? 300 : 0);
-            });
-        };
-
-
-
-        for (let i = 0; i < btns.length; i++) {
-            let btn = btns[i];
-            let menuButton = new FAButton(btn.Icon, "white", "blue");;
-
-            menuButton.x = piece.canvasReference.x;
-            menuButton.y = piece.canvasReference.y;
-
-            stage.addChild(menuButton);
-
-            menuButton.on("mouseover", function() {
-                //  menuButton.btn.graphics.clear().beginFill("blue");
-                createjs.Tween.get(menuButton, {
-                    scaleX: 1,
-                    scaleY: 1
-                }).to({
-                    scaleX: 1.25,
-                    scaleY: 1.25
-                }, 100, createjs.Ease.getPowInOut(4));
-
-            });
-
-            menuButton.on("mouseout", function() {
-                //menuButton.btn.graphics.clear().beginFill("red");
-                createjs.Tween.get(menuButton, {
-                    scaleX: 1.25,
-                    scaleY: 1.25
-                }).to({
-                    scaleX: 1,
-                    scaleY: 1
-                }, 100, createjs.Ease.getPowInOut(4));
-            });
-
-            stage.addChild(menuButton);
-
-            let toX = 0;
-            let toY = 0;
-            switch (orientation) {
-                case "circle":
-                    toX = piece.canvasReference.x - (30 * Math.cos(i * (Math.PI / 4)));
-                    toY = piece.canvasReference.y - (30 * Math.sin(i * (Math.PI / 4)));
-                    break;
-                case "up":
-                    toX = piece.canvasReference.x;
-                    toY = piece.canvasReference.y - (15 * (i + 1));
-                    break;
-                case "down":
-                    toX = piece.canvasReference.x;
-                    toY = piece.canvasReference.y + (15 * (i + 1));
-                    break;
-                case "left":
-                    toX = piece.canvasReference.x - (15 * (i + 1));
-                    toY = piece.canvasReference.y;
-                    break;
-                case "right":
-                    toX = piece.canvasReference.x + (15 * (i + 1));
-                    toY = piece.canvasReference.y;
-                    break;
-                default:
-
-            }
-
-            stage.setChildIndex(menuButton, 0);
-
+    function hide(clickedIndex) {
+        menuButtons.forEach(function(btn, index) {
             setTimeout(function() {
-                createjs.Tween.get(menuButton, {
+                createjs.Tween.get(btn, {
                     loop: false
                 }).to({
-                    x: toX,
-                    y: toY
+                    x: displayObject.x,
+                    y: displayObject.y
                 }, 1000, createjs.Ease.getPowInOut(4));
-            }, 50 * i);
+            }, clickedIndex == index ? 300 : 0);
 
+            setTimeout(function() {
+                stage.removeChild(btn);
+            }, 1000);
+        });
+    };
 
-            menuButtons.push(menuButton);
-            gamePieces.push(menuButton);
+    for (let i = 0; i < btns.length; i++) {
+        let btn = btns[i];
+        let menuButton = new FAButton(btn.Icon, "white", "blue");;
 
-            menuButton.canvasReference = menuButton;
-            menuButton.on("click", function() {
-                if (btn.click()) {
-                    hide(i);
-                }
-                //hide(i);
-                // $(window).on("mousemove", function(){
-                //   beginDragNode(evt);
-                // });
-            });
+        menuButton.x = displayObject.x;
+        menuButton.y = displayObject.y;
+
+        stage.addChild(menuButton);
+
+        menuButton.on("mouseover", function() {
+            //  menuButton.btn.graphics.clear().beginFill("blue");
+            createjs.Tween.get(menuButton, {
+                scaleX: 1,
+                scaleY: 1
+            }).to({
+                scaleX: 1.25,
+                scaleY: 1.25
+            }, 100, createjs.Ease.getPowInOut(4));
+
+        });
+
+        menuButton.on("mouseout", function() {
+            //menuButton.btn.graphics.clear().beginFill("red");
+            createjs.Tween.get(menuButton, {
+                scaleX: 1.25,
+                scaleY: 1.25
+            }).to({
+                scaleX: 1,
+                scaleY: 1
+            }, 100, createjs.Ease.getPowInOut(4));
+        });
+
+        stage.addChild(menuButton);
+
+        let toX = 0;
+        let toY = 0;
+
+        var displayObjectBoundsWidth = displayObject.getBounds().width;
+
+        console.log(displayObjectBoundsWidth);
+        switch (orientation) {
+            case "circle":
+                toX = displayObject.x - ((displayObjectBoundsWidth * 2) * Math.cos(i * (Math.PI / 4)));
+                toY = displayObject.y - ((displayObjectBoundsWidth * 2) * Math.sin(i * (Math.PI / 4)));
+                break;
+            case "up":
+                toX = displayObject.x;
+                toY = displayObject.y - ((displayObjectBoundsWidth * 2) * (i + 1));
+                break;
+            case "down":
+                toX = displayObject.x;
+                toY = displayObject.y + ((displayObjectBoundsWidth * 2) * (i + 1));
+                break;
+            case "left":
+                toX = displayObject.x - ((displayObjectBoundsWidth * 2) * (i + 1));
+                toY = displayObject.y;
+                break;
+            case "right":
+                toX = displayObject.x + ((displayObjectBoundsWidth * 2) * (i + 1));
+                toY = displayObject.y;
+                break;
+            default:
+
         }
 
-    });
+        stage.setChildIndex(menuButton, 0);
+
+        setTimeout(function() {
+            createjs.Tween.get(menuButton, {
+                loop: false
+            }).to({
+                x: toX,
+                y: toY
+            }, 1000, createjs.Ease.getPowInOut(4));
+        }, 50 * i);
+
+
+        menuButtons.push(menuButton);
+
+        if (btn.click) {
+            menuButton.on("click", function() {
+                if (btn.click(menuButton)) {
+                    hide(i);
+                }
+            });
+        }
+    }
 }
