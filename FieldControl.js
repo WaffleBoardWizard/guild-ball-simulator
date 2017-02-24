e9e47b1a9dcd12defbc2b175e55cf0e75b7d37e3
Andrew <Andrew>
2017-02-24 14:43:30 -0600

    Added Field Control

---
+++ FieldControl.js
@@ -0,0 +1,28 @@
+(function() {
+    var initialSize = 1200;
+
+    function FieldControl(icon, iconColor, bgColor) {
+        this.Container_constructor();
+
+        var image = new Image();
+        image.src = "./assets/field.jpg";
+        var me = this;
+        image.onload = function() {
+            var image = event.target;
+            me.bitmap = new createjs.Bitmap(image);
+            me.addChild(me.bitmap);
+        };
+
+    };
+
+    var p = createjs.extend(FieldControl, createjs.Container);
+
+    p.SetScale = function(scale) {
+      var scaleTimes = 0;
+        scaleTimes = 1200 / scale;
+
+      //  this.bitmap.scaleX = this.bitmap.scaleY = scaleTimes;
+    }
+
+    window.FieldControl = createjs.promote(FieldControl, "Container");
+}());
