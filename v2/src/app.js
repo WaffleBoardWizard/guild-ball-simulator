67ae2eaabd5ef85c79119dd991f72756c526a92b
unknown <apheim@gmail.com>
2017-02-27 21:01:34 -0600

    got images working

--- v2/src/app.js
+++ v2/src/app.js
@@ -1,19 +1,20 @@
 import $ from 'jquery';
-//import createjs from './lib/createjs.min.js';
 import proton from './lib/proton.min.js';
 import FieldControl from './controls/FieldControl';
 import Measurement from './common/Measurements';
+const AssetsDirectory = require.context('./assets', false);
+
 var stage, field, ball, proton;
 var dragger;
 var foot = 400;
 var inch = foot / 12;
 var assetsToLoad = [
-  'ballista.png',
-  "colossus.png",
-  "hoist.png",
-  "mainspring.png",
-  "ratchet.png",
-  "salvo.png",
+  'ballista.PNG',
+  "hoist.PNG",
+  "colossus.PNG",
+  "mainspring.PNG",
+  "ratchet.PNG",
+  "salvo.PNG",
   "field.jpg",
   "die.png",
   "ball.png",
@@ -36,20 +37,22 @@ function init(){
     stage = new createjs.Stage("demoCanvas");
     stage.preventSelection = false;
     createjs.Touch.enable(stage, false, true);
-    field = new FieldControl();
+    field = new FieldControl(assets.field);
     stage.addChild(field);
+
+    stage.update();
     //createProton();
     //loadGame();
   });
 }
 
+
 function loadAssets(onSuccess) {
   var assetsLoaded = 0;
   assetsToLoad.forEach(function(asset) {
     var image = new Image();
-    var path = './assets/' + asset;
-    console.log('./assets/ballista.png')
-    image.src =  path;
+    var path = './' + asset;
+    image.src =  AssetsDirectory(path, false);
     var me = this;
     image.onload = function() {
       var imageName = asset.split(".")[0];
